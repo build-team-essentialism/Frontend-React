8e441a9d8a4b8580bc1bcdea2be83ace79dd4afc
vpagano10 <vpagano10@gmail.com>
2019-11-20 10:32:45 -0500

    added files to edit prompts and pillars

---
+++ essentialism/src/components/PillarEdits.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import api from '../utils/api';
+
+function PillarEdits(props) {
+    const [pillar, setPillar] = useState({
+        id: '',
+        pillar: ''
+    });
+
+    useEffect(() => {
+        api()
+            .get(`/api/pillars/${props.match.params.id}`)
+            .then(res => {
+                setPillar(res.data)
+            })
+            .catch(err => {
+                console.log('Pillar edit error', err)
+            });
+    }, [props.match.params.id]);
+
+    const handleChange = (event) => {
+        setPillar({
+            ...pillar,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        api()
+            .put(`/api/pillars/${props.match.params.id}`, pillar)
+            .then(res => {
+                props.history.push('')
+            })
+            .catch(err => {
+                console.log('Error with edit put req', err)
+            });
+    };
+
+    return (
+        <>
+            <h1>Update Pillar</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='pillar'
+                /><br />
+                <button type='submit'>Save</button>
+            </form>
+        </>
+    );
+};
+
+export default PillarEdits;
---
+++ essentialism/src/components/PromptEdits.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import api from '../utils/api';
+
+function PromptEdits(props) {
+    const [prompt, setPrompt] = useState({
+        id: '',
+        prompt: ''
+    });
+
+    useEffect(() => {
+        api()
+            .get(`/api/prompts/${props.match.params.id}`)
+            .then(res => {
+                setPrompt(res.data)
+            })
+            .catch(err => {
+                console.log('Prompt edit error', err)
+            });
+    }, [props.match.params.id]);
+
+    const handleChange = (event) => {
+        setPrompt({
+            ...pillar,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        api()
+            .put(`/api/prompts/${props.match.params.id}`, prompt)
+            .then(res => {
+                props.history.push('')
+            })
+            .catch(err => {
+                console.log('Error with edit put req', err)
+            });
+    };
+
+    return (
+        <>
+            <h1>Update Prompt</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='prompt'
+                /><br />
+                <button type='submit'>Save</button>
+            </form>
+        </>
+    );
+};
+
+export default PromptEdits;
